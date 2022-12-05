193eda9265e912face51259ae01bd8832d405aa7
khomabhena <colwanymab@gmail.com>
2022-12-05 20:02:28 +0200

    Get coordinates from the browser

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const ApiCalls = () => {
-    const API_KEY = ''
+const GeoLocation = () => {
+    let lat = ''
+    let lon = ''
+
+    const getCoordinates = () => {
+        if (navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(showPosition)
+    }
+
+    const showPosition = (position) => {
+        lat = position.coords.latitude
+        lon = position.coords.longitude
+
+        console.log(`Lat: ${lat} \nLong: ${lon}`)
+    } 
+
+    return { getCoordinates }
 }
+
+
+GeoLocation().getCoordinates()
